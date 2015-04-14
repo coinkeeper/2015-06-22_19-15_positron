c6fce217e4ac510188f524bb2598a9c9f3b4f869
hoardcoin <hoardcoin@mail.com>
2015-04-14 00:22:07 -0400

    Checkpoint update fix

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -27,8 +27,9 @@ namespace Checkpoints
     //
     static MapCheckpoints mapCheckpoints =
         boost::assign::map_list_of
-       ( 0,      hashGenesisBlock )
+      ( 0,      hashGenesisBlock )
       ( 3253, uint256("0x211f5f4e60881180f7c3b538309de2e93f5b9aa85504fab89697ef54f7fc6acf"))
+      ( 3306, uint256("0x8fbf7f2aa8cda44599e358c21ee5c44eadd7bd868400312ea1bea71c578e5b5d"))
     ;
 
     // TestNet has no checkpoints
